beda1b68fafb46fa422c9ba67e69ddb30bc898b5
Amarnath Kumar <amarnathkumar1455@gmail.com>
2025-07-05 01:07:44 +0530

    initRepo logic

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,5 +1,21 @@
-function initRepo() {
-  console.log("Init command called");
+const fs = require("fs").promises;
+const path = require("path");
+
+async function initRepo() {
+  const repoPath = path.resolve(process.cwd(), ".codehub");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    await fs.mkdir(repoPath, { recursive: true });
+    await fs.mkdir(commitsPath, { recursive: true });
+    await fs.writeFile(
+      path.join(repoPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET })
+    );
+    console.log("Repository initialized successfully");
+  } catch (err) {
+    console.error("Error initializing repository:", err);
+  }
 }
 
 module.exports = { initRepo };
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const { hideBin } = require("yargs/helpers");
 
 const { initRepo } = require("./controllers/init");
 const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
   .command("init", "Initialize a new repository", {}, initRepo)
@@ -16,6 +20,30 @@ yargs(hideBin(process.argv))
       });
     },
     addRepo
+  )
+  .command(
+    "commit <message>",
+    "Commit the staged files",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
+      });
+    },
+    commitRepo
+  )
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits from S3", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revet to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "Commit ID to revert to",
+        type: "string",
+      });
+    },
+    revertRepo
   )
   .demandCommand(1, "You need at least one command before moving on")
   .help().argv;
